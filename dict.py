49ddb7d2c5b5135b40f1ac631511f0957ee33c7f
maddz131 <maddz131@gmail.com>
2022-02-17 19:56:51 -0800

    Complete Refactor:Updated code to use the json that the Webster API now returns(previously XML). Switched from Kivy to PySimpleGui. Expanded words in WordsList.txt

--- dict.py
+++ dict.py
@@ -1,109 +1,124 @@
 import requests
 import random
-import xml.etree.ElementTree as ET
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.graphics import Rectangle, Color
+import PySimpleGUI as sg
+import json
+import textwrap
+
+def getWord(advanced_words, common_words, word_list_length):
+    while True:
+        num = random.randint(0, word_list_length)
+        chosen_word = advanced_words[num]
+        if chosen_word not in common_words and not chosen_word[0].isupper():
+            return chosen_word
+
+def getDefinition(word):
+    KEY = "8e964aef-279a-4bd2-847f-cd34a2cfe4d9" #used in API call
+    requestURL = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'+ str(word) +'?key=' + KEY # str(chosen_word) 
+    apiResponse = requests.get(requestURL) #http GET request of URL using requests library
+    return json.loads(apiResponse.content)
+
+def parseInfo(word_entry):
+    definition_dict = {}
+    definitions = []
+    print(word_entry)
+    try:
+        definitions.append(word_entry['fl'])  #fl is the functional label aka class of the word
+        print(word_entry['fl'])
+    except Exception as e:
+        definitions.append('No Class Type Specified')
+        print("KeyError: fl")
+
+    for index, definition in enumerate(word_entry['shortdef']):
+        definition_dict[index+1] = definition
+
+    definitions.append(definition_dict)
+    return(definitions)
+
+def generateWordAndDefenition():
+    with open("wordsList.txt", "r") as read_file:
+        advanced_words = read_file.read().split()   #taken from "https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co"
+        common_words = open("commonWords.txt").read().split()  #list of common words that we users would already know
+    chosen_word = getWord(advanced_words, common_words, len(advanced_words))
+    print(chosen_word)
+    word_info = getDefinition(chosen_word)
+    if not any(isinstance(info, dict) for info in word_info): #if the api response isn't a dictionary, that means it could find the word and we need a new one
+        word_with_definitions = generateWordAndDefenition()
+    else:
+        word_with_definitions = [chosen_word]
+        definitions_list = []
+        if 'hom' in word_info[0]: #if there are multiple hominims print them, otherwise just print the main definition
+            definitions_list = [parseInfo(entry) for entry in word_info if entry.get('shortdef') and entry.get('hom')] #.get() checks that the hom and shortdef 1) exist and 2) are not null
+        else:
+            print(word_info)
+            if word_info[0].get('shortdef'):
+                definitions_list  = [parseInfo(word_info[0])]
+
+        if not definitions_list:    #if the chosen word didn't have a usable definition, get another
+            word_with_definitions = getDefinition()
+        else:
+            word_with_definitions.append(definitions_list)
+            print(word_with_definitions)
+
+    return word_with_definitions
+
+def formatDefinitions(class_definitions):
+    formatted_definitions = ''
+    label = ['a','b','c','d','e','f','g','h','i'] #not ideal, but assumes there will be no more than 9 definitions per class
+
+    for index, definition in enumerate(class_definitions):
+        formatted_definitions += "{}) ".format(label[index])
+        longtext = textwrap.wrap(class_definitions[definition], 80)
+        for text in longtext:
+            formatted_definitions += "{} \n".format(text)
+    return formatted_definitions
 
 
+#PROGRAM STARTING POINT
+def main():
+    column1 = [ [sg.Text('Welcome to the Word of the Day', key = '_intro_', font = 'Bold')],
+                [sg.Text(key = '_text2_', visible = False)],
+                [sg.Button('Generate Word'), sg.Exit()] ]
+    layout = [[sg.Column(column1, size = (600,500))]]
 
-total = ""
-origin = ""
-entry = None
-KEY = "8e964aef-279a-4bd2-847f-cd34a2cfe4d9"
+    window = sg.Window("Word of the Day", layout, finalize = True) # Create the window
 
-word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
-#word_site = "https://gist.githubusercontent.com/deekayen/4148741/raw/01c6252ccc5b5fb307c1bb899c95989a8a284616/1-1000.txt"
-wordResponse = requests.get(word_site)
-WORDS = wordResponse.content.splitlines()   #list of words from site
-length = len(WORDS)         # num of words in the file
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED: # End program if user closes window or presses the Exit button
+            break
 
-WCHECK = open("commonWords.txt").read().split()     #list of common words that we users would already know
+        if event == "Generate Word":
+            word_definition = generateWordAndDefenition()
+            text1 = word_definition[0] #the word
+            text2 = '' #the definition
+            classNum = '' #for words with multiple classes (nouns, adjectives, etc.)
+            for i in range(len(word_definition[1])):
+                if len(word_definition[1])>1:
+                    classNum = f'{i+1}: '
+                text2 += (f'{classNum}{word_definition[1][i][0]}\n{formatDefinitions(word_definition[1][i][1])}\n\n')
+            window['_intro_'].update(text1)
+            window['_text2_'].update(text2, visible = True)
 
-def getWord(length): #gets a random word from the file
-    check = True
-    while(check==True):
-        check = False
-        index = random.randint(1,length)
-        for word in WCHECK:     #checks that word from file is not a 'common word'
-            if WORDS[index] == word:
-                check = True     #if it is common, loop will continue until it gets uncommon word
-                print(word)
+if __name__ == "__main__":
+    main()
 
-    return index    #index of acceptable word is returned
+''' 
+Note:
+    An example of a word_definition is:
+    ['advance',
+        [
+            ['verb', {1: 'to accelerate the growth or progress of', 2: 'to bring or move forward', 3: 'to raise to a higher rank'}],
+            ['noun', {1: 'a moving forward', 2: 'progress in development', 3: 'a progressive step : improvement'}],
+            ['adjective', {1: 'made, sent, or furnished ahead of time', 2: 'going or situated before'}]
+        ]
+    ]
 
-def getInfo(index): #gets info on the word from webster api
-    chosen = WORDS[index].decode('utf-8') #converts the bytes object to string
-    requestURL = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'+ chosen +'?key=' + KEY #WORDS[index]
-    dictResponse = requests.get(requestURL) #http GET request of URL using requests library
-    xml = dictResponse.content #gets content of GET response which is xml
-    root = ET.fromstring(xml)  #uses element tree library to convert xml into searchable tree
-    return root
-
-def mainfunction(total,origin,entry):
-    #if no entry file exists, or if the entry is a location, get a new word and its merriam-webster info
-    while(entry is None or entry.find('fl').text == "geographical name" or entry.find('fl').text == "biographical name" or entry.find('fl').text == "abbreviation"): #(entry is None) or
-        index = getWord(length)  #gets a index of random acceptable word from the file and assigns it to index
-        root = getInfo(index)    #gets xml tree of chosen word (found using index)
-        entry = root.find('entry')    #'entry' branch of xml tree contains all other branches, so I dont have to write root.find('entry').find('pr')
-
-    #print fields
-    word = entry.find('ew').text
-    total +=  "\nWord: " + word#print("\nWord: " + word)
-
-    if entry.find('pr') is not None:
-        pr = entry.find('pr').text
-        prlabel = "\nPronunciation: " #print("Pronunciation: " +  pr)
-
-    if entry.find('fl') is not None:
-        wt = entry.find('fl').text
-        total += "\nWord Type: " +  wt#print("Word Type: " +  wt)
-
-    if entry.find('et') is not None:
-        origin = entry.find('et').text
-        total += "\nOrigin: " + str(origin)#print("Origin: " + str(origin))
-
-    defs = entry.find('def')
-    total += "\nDefinition:"#print "Definition:"
-    #print "".join(defs.itertext())
-    for count, dt in enumerate(defs.iter('dt'), start = 1): #iterate through all dt's(definitions) in defs
-        total += "\n{}{}".format(count, "".join(dt.itertext()))#print "{}{}".format(count, "".join(dt.itertext())) #"".join(dt.itertext()) makes sure all branches within dt are also printed
-    return total
-    #gui(word, pr, wt, origin)
-
-class Hello(RelativeLayout):
-    def __init__(self,**kwargs):
-        super(Hello,self).__init__(**kwargs)
-        #self.l = Label(text=mainfunction(total,origin,entry),text_size=(None,None),
-        #                      font_size="20sp",
-        #                      pos_hint={'center_x': 0.5, 'center_y': .85},
-        #                      size_hint_y=None,
-        #                      height=self.texture_size[1],
-        #                      halign="center",
-        #                      valign = "middle",
-        #                      color=(0.055, 0.235, 0.541, 1))
-        self.l = Label(text=mainfunction(total,origin,entry))
-        self.b = Button(text = "Next", size_hint=(.3, .1), on_press=self.update)
-        self.add_widget(self.l)
-        self.add_widget(self.b)
-    def update(self,event):
-        self.l.text = mainfunction(total,origin,entry)
-
-class WordApp(App):
-    def build(self):
-        return Hello()
-        #l = Label(text=mainfunction(total,origin,entry))
-        #b = Button(text = "Next")
-        #b.bind(on_press=self.update)
-        #layout = BoxLayout(orientation='vertical')
-        #layout.add_widget(l)
-        #layout.add_widget(b)
-        #return layout
-
-    #def update():
-    #    l.text = mainfunction(total,origin,entry)
-
-WordApp().run()
+    Each word can have many classes (verb, noun, adjective, etc.) and each class is in it's own list along with it's corresponding definition(s).
+    So each word list has:
+        - word_definition[0]: a string of the word
+        - word_definition[1]: a list made up of n number of lists (word_definition[1][n], one list per each class that the word has)
+    Word_definition[1][n] is a list containing two elements:
+        - word_definition[1][n][0]: a string specifying the class (verb, adj, noun, etc.) 
+        - word_definition[1][n][1]: a dictionary of the definitions, because a single class can still have multiple definitions.
+          The key is the definition number and the value is the defintition
+'''
