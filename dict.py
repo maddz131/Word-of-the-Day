e071e90cffac4690c08d14868e58180a9a16b441
maddz131 <maddz131@gmail.com>
2022-03-28 17:28:39 -0700

    Made the word an object for a more OOP approach.

--- dict.py
+++ dict.py
@@ -4,9 +4,17 @@ import PySimpleGUI as sg
 import json
 import textwrap
 
-def getWord(advanced_words, common_words, word_list_length):
+class Word:
+    def __init__(self, name):
+        self.name = name
+        self.definitions = []
+
+def getWord():
+    with open("wordsList.txt", "r") as read_file:
+        advanced_words = read_file.read().split()   #taken from "https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co"
+        common_words = open("commonWords.txt").read().split()  #list of common words that we users would already know
     while True:
-        num = random.randint(0, word_list_length)
+        num = random.randint(0, len(advanced_words))
         chosen_word = advanced_words[num]
         if chosen_word not in common_words and not chosen_word[0].isupper():
             return chosen_word
@@ -33,29 +41,23 @@ def parseInfo(word_entry):
     return(definitions)
 
 def generateWordAndDefenition():
-    with open("wordsList.txt", "r") as read_file:
-        advanced_words = read_file.read().split()   #taken from "https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co"
-        common_words = open("commonWords.txt").read().split()  #list of common words that we users would already know
-    chosen_word = getWord(advanced_words, common_words, len(advanced_words))
+    chosen_word = getWord()
     print(chosen_word)
     word_info = getDefinition(chosen_word)
     if not any(isinstance(info, dict) for info in word_info): #if the api response isn't a dictionary, that means it couldn't find the word and we need a new one
-        word_with_definitions = generateWordAndDefenition()
+        ourWord = generateWordAndDefenition()
     else:
-        word_with_definitions = [chosen_word]
+        ourWord = Word(chosen_word)
         definitions_list = []
         if 'hom' in word_info[0]: #if there are multiple homonym print them, otherwise just print the main definition
             definitions_list = [parseInfo(entry) for entry in word_info if entry.get('shortdef') and entry.get('hom')] #.get() checks that the hom and shortdef 1) exist and 2) are not null
-        else:
-            print(word_info)
-            if word_info[0].get('shortdef'):
-                definitions_list  = [parseInfo(word_info[0])]
-        if not definitions_list:    #if the chosen word didn't have a usable definition, get another
-            word_with_definitions = generateWordAndDefenition()
-        else:
-            word_with_definitions.append(definitions_list)
-            print(word_with_definitions)
-    return word_with_definitions
+            ourWord.definitions = definitions_list
+        elif word_info[0].get('shortdef'):
+            definitions_list  = [parseInfo(word_info[0])]
+            ourWord.definitions = definitions_list
+        else:    #if the chosen word didn't have a usable definition, get another
+            ourWord = generateWordAndDefenition()
+    return ourWord
 
 def formatDefinitions(class_definitions):
     formatted_definitions = ''
@@ -79,14 +81,14 @@ def main():
         if event == "Exit" or event == sg.WIN_CLOSED: # End program if user closes window or presses the Exit button
             break
         if event == "Generate Word":
-            word_definition = generateWordAndDefenition()
-            text1 = word_definition[0] #the word
+            word = generateWordAndDefenition()
+            text1 = word.name #the word
             text2 = '' #the definition
             classNum = '' #for words with multiple classes (nouns, adjectives, etc.)
-            for i in range(len(word_definition[1])):
-                if len(word_definition[1])>1:
+            for i in range(len(word.definitions)): #word_definition[1]
+                if len(word.definitions)>1:
                     classNum = f'{i+1}: '
-                text2 += (f'{classNum}{word_definition[1][i][0]}\n{formatDefinitions(word_definition[1][i][1])}\n\n')
+                text2 += (f'{classNum}{word.definitions[i][0]}\n{formatDefinitions(word.definitions[i][1])}\n\n')
             window['_intro_'].update(text1)
             window['_text2_'].update(text2, visible = True)
 
@@ -96,12 +98,10 @@ if __name__ == "__main__":
 ''' 
 Note:
     An example of a word_definition is:
-    ['advance',
-        [
-            ['verb', {1: 'to accelerate the growth or progress of', 2: 'to bring or move forward', 3: 'to raise to a higher rank'}],
-            ['noun', {1: 'a moving forward', 2: 'progress in development', 3: 'a progressive step : improvement'}],
-            ['adjective', {1: 'made, sent, or furnished ahead of time', 2: 'going or situated before'}]
-        ]
+    [
+        ['verb', {1: 'to accelerate the growth or progress of', 2: 'to bring or move forward', 3: 'to raise to a higher rank'}],
+        ['noun', {1: 'a moving forward', 2: 'progress in development', 3: 'a progressive step : improvement'}],
+        ['adjective', {1: 'made, sent, or furnished ahead of time', 2: 'going or situated before'}]
     ]
 
     Each word can have many classes (verb, noun, adjective, etc.) and each class is in it's own list along with it's corresponding definition(s).
